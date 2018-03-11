99453d8106e93a99090ea6f27263c63c85524d78
jm <johnmichaelmaytona@gmail.com>
2018-03-10 18:30:03 -0800

    Starting VB.NET 2017

---
+++ tutorial1/Basics.Designer.vb
@@ -0,0 +1,188 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Basics
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.lblName = New System.Windows.Forms.Label()
+        Me.txtbxName = New System.Windows.Forms.TextBox()
+        Me.btnOk = New System.Windows.Forms.Button()
+        Me.btnMessage = New System.Windows.Forms.Button()
+        Me.btnExit = New System.Windows.Forms.Button()
+        Me.txtNum1 = New System.Windows.Forms.TextBox()
+        Me.txtNum2 = New System.Windows.Forms.TextBox()
+        Me.btnAdd = New System.Windows.Forms.Button()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.txtNumResult = New System.Windows.Forms.TextBox()
+        Me.btnSubtract = New System.Windows.Forms.Button()
+        Me.btnMultiply = New System.Windows.Forms.Button()
+        Me.btnDivide = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'lblName
+        '
+        Me.lblName.AutoSize = True
+        Me.lblName.Location = New System.Drawing.Point(12, 9)
+        Me.lblName.Name = "lblName"
+        Me.lblName.Size = New System.Drawing.Size(38, 13)
+        Me.lblName.TabIndex = 0
+        Me.lblName.Text = "Name:"
+        '
+        'txtbxName
+        '
+        Me.txtbxName.Location = New System.Drawing.Point(56, 6)
+        Me.txtbxName.Name = "txtbxName"
+        Me.txtbxName.Size = New System.Drawing.Size(141, 20)
+        Me.txtbxName.TabIndex = 1
+        '
+        'btnOk
+        '
+        Me.btnOk.Location = New System.Drawing.Point(26, 32)
+        Me.btnOk.Name = "btnOk"
+        Me.btnOk.Size = New System.Drawing.Size(35, 23)
+        Me.btnOk.TabIndex = 2
+        Me.btnOk.Text = "&Ok"
+        Me.btnOk.UseVisualStyleBackColor = True
+        '
+        'btnMessage
+        '
+        Me.btnMessage.Location = New System.Drawing.Point(67, 32)
+        Me.btnMessage.Name = "btnMessage"
+        Me.btnMessage.Size = New System.Drawing.Size(75, 23)
+        Me.btnMessage.TabIndex = 3
+        Me.btnMessage.Text = "&Message"
+        Me.btnMessage.UseVisualStyleBackColor = True
+        '
+        'btnExit
+        '
+        Me.btnExit.Location = New System.Drawing.Point(148, 32)
+        Me.btnExit.Name = "btnExit"
+        Me.btnExit.Size = New System.Drawing.Size(53, 23)
+        Me.btnExit.TabIndex = 4
+        Me.btnExit.Text = "E&xit"
+        Me.btnExit.UseVisualStyleBackColor = True
+        '
+        'txtNum1
+        '
+        Me.txtNum1.Location = New System.Drawing.Point(26, 71)
+        Me.txtNum1.Name = "txtNum1"
+        Me.txtNum1.Size = New System.Drawing.Size(35, 20)
+        Me.txtNum1.TabIndex = 5
+        '
+        'txtNum2
+        '
+        Me.txtNum2.Location = New System.Drawing.Point(67, 71)
+        Me.txtNum2.Name = "txtNum2"
+        Me.txtNum2.Size = New System.Drawing.Size(35, 20)
+        Me.txtNum2.TabIndex = 6
+        '
+        'btnAdd
+        '
+        Me.btnAdd.Location = New System.Drawing.Point(26, 97)
+        Me.btnAdd.Name = "btnAdd"
+        Me.btnAdd.Size = New System.Drawing.Size(24, 23)
+        Me.btnAdd.TabIndex = 7
+        Me.btnAdd.Text = "+"
+        Me.btnAdd.UseVisualStyleBackColor = True
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(108, 74)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(13, 13)
+        Me.Label1.TabIndex = 8
+        Me.Label1.Text = "="
+        '
+        'txtNumResult
+        '
+        Me.txtNumResult.BackColor = System.Drawing.SystemColors.ActiveCaption
+        Me.txtNumResult.Location = New System.Drawing.Point(127, 71)
+        Me.txtNumResult.Name = "txtNumResult"
+        Me.txtNumResult.Size = New System.Drawing.Size(35, 20)
+        Me.txtNumResult.TabIndex = 9
+        '
+        'btnSubtract
+        '
+        Me.btnSubtract.Location = New System.Drawing.Point(56, 97)
+        Me.btnSubtract.Name = "btnSubtract"
+        Me.btnSubtract.Size = New System.Drawing.Size(24, 23)
+        Me.btnSubtract.TabIndex = 10
+        Me.btnSubtract.Text = "-"
+        Me.btnSubtract.UseVisualStyleBackColor = True
+        '
+        'btnMultiply
+        '
+        Me.btnMultiply.Location = New System.Drawing.Point(86, 97)
+        Me.btnMultiply.Name = "btnMultiply"
+        Me.btnMultiply.Size = New System.Drawing.Size(24, 23)
+        Me.btnMultiply.TabIndex = 11
+        Me.btnMultiply.Text = "*"
+        Me.btnMultiply.UseVisualStyleBackColor = True
+        '
+        'btnDivide
+        '
+        Me.btnDivide.Location = New System.Drawing.Point(116, 97)
+        Me.btnDivide.Name = "btnDivide"
+        Me.btnDivide.Size = New System.Drawing.Size(24, 23)
+        Me.btnDivide.TabIndex = 12
+        Me.btnDivide.Text = "/"
+        Me.btnDivide.UseVisualStyleBackColor = True
+        '
+        'Basics
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(220, 149)
+        Me.Controls.Add(Me.btnDivide)
+        Me.Controls.Add(Me.btnMultiply)
+        Me.Controls.Add(Me.btnSubtract)
+        Me.Controls.Add(Me.txtNumResult)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.btnAdd)
+        Me.Controls.Add(Me.txtNum2)
+        Me.Controls.Add(Me.txtNum1)
+        Me.Controls.Add(Me.btnExit)
+        Me.Controls.Add(Me.btnMessage)
+        Me.Controls.Add(Me.btnOk)
+        Me.Controls.Add(Me.txtbxName)
+        Me.Controls.Add(Me.lblName)
+        Me.Name = "Basics"
+        Me.Text = "Basics"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents lblName As Label
+    Friend WithEvents txtbxName As TextBox
+    Friend WithEvents btnOk As Button
+    Friend WithEvents btnMessage As Button
+    Friend WithEvents btnExit As Button
+    Friend WithEvents txtNum1 As TextBox
+    Friend WithEvents txtNum2 As TextBox
+    Friend WithEvents btnAdd As Button
+    Friend WithEvents Label1 As Label
+    Friend WithEvents txtNumResult As TextBox
+    Friend WithEvents btnSubtract As Button
+    Friend WithEvents btnMultiply As Button
+    Friend WithEvents btnDivide As Button
+End Class
---
+++ tutorial1/Basics.vb
@@ -0,0 +1,89 @@
+﻿Public Class Basics
+    Private Sub Basics_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+
+    Private Sub lblName_Click(sender As Object, e As EventArgs) Handles lblName.Click
+
+    End Sub
+
+    Private Sub btnOk_Click(sender As Object, e As EventArgs) Handles btnOk.Click
+        txtbxName.Text = "Hello " & txtbxName.Text & "!"
+
+    End Sub
+
+    Private Sub TextBox1_TextChanged(sender As Object, e As EventArgs) Handles txtbxName.TextChanged
+
+    End Sub
+
+    Private Sub btnMessage_Click(sender As Object, e As EventArgs) Handles btnMessage.Click
+        MessageBox.Show("Hello " & txtbxName.Text)
+
+    End Sub
+
+    Private Sub btnExit_Click(sender As Object, e As EventArgs) Handles btnExit.Click
+        Me.Close()
+
+    End Sub
+
+    Private Sub btnAdd_Click(sender As Object, e As EventArgs) Handles btnAdd.Click
+
+        Try
+            Dim firstNum As Integer = CInt(txtNum1.Text)
+            Dim secondNum As Integer = CInt(txtNum2.Text)
+            Dim result = firstNum + secondNum
+
+            txtNumResult.Text = CStr(result)
+        Catch ex As System.InvalidCastException
+            MessageBox.Show("Please enter a number", "Error")
+
+        Catch ex As Exception
+            MessageBox.Show("Unknow error occured", "Error")
+        End Try
+    End Sub
+
+    Private Sub btnSubtract_Click(sender As Object, e As EventArgs) Handles btnSubtract.Click
+        Try
+            Dim firstNum As Integer = CInt(txtNum1.Text)
+            Dim secondNum As Integer = CInt(txtNum2.Text)
+            Dim result = firstNum - secondNum
+
+            txtNumResult.Text = CStr(result)
+        Catch ex As System.InvalidCastException
+            MessageBox.Show("Please enter a number", "Error")
+
+        Catch ex As Exception
+            MessageBox.Show("Unknow error occured", "Error")
+        End Try
+    End Sub
+
+    Private Sub btnMultiply_Click(sender As Object, e As EventArgs) Handles btnMultiply.Click
+        Try
+            Dim firstNum As Integer = CInt(txtNum1.Text)
+            Dim secondNum As Integer = CInt(txtNum2.Text)
+            Dim result = firstNum * secondNum
+
+            txtNumResult.Text = CStr(result)
+        Catch ex As System.InvalidCastException
+            MessageBox.Show("Please enter a number", "Error")
+
+        Catch ex As Exception
+            MessageBox.Show("Unknow error occured", "Error")
+        End Try
+    End Sub
+
+    Private Sub btnDivide_Click(sender As Object, e As EventArgs) Handles btnDivide.Click
+        Try
+            Dim firstNum As Integer = CInt(txtNum1.Text)
+            Dim secondNum As Integer = CInt(txtNum2.Text)
+            Dim result = firstNum / secondNum
+
+            txtNumResult.Text = CStr(result)
+        Catch ex As System.InvalidCastException
+            MessageBox.Show("Please enter a number", "Error")
+
+        Catch ex As Exception
+            MessageBox.Show("Unknow error occured", "Error")
+        End Try
+    End Sub
+End Class
